34df498777464d3a15580162a597ef71300f3725
Grace <govlmo91@gmail.com>
2020-09-21 05:39:03 +0900

    init

---
+++ src/main/java/kr/gracelove/springreactdemo/SpringReactDemoApplication.java
@@ -0,0 +1,13 @@
+package kr.gracelove.springreactdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/gracelove/springreactdemo/SpringReactDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.gracelove.springreactdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
